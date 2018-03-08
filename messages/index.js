4ada166ed75be8e230249c8172cb537675152142
Adam Stankiewicz <sheerun@sher.pl>
2018-03-08 01:59:26 +0100

    Init

---
+++ messages/index.js
@@ -0,0 +1,58 @@
+// For more information about this template visit http://aka.ms/azurebots-node-qnamaker
+
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var builder_cognitiveservices = require("botbuilder-cognitiveservices");
+var path = require('path');
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+  appId: process.env['MicrosoftAppId'],
+  appPassword: process.env['MicrosoftAppPassword'],
+  openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+/*----------------------------------------------------------------------------------------
+ * Bot Storage: This is a great spot to register the private state storage for your bot. 
+ * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+ * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+ * ---------------------------------------------------------------------------------------- */
+
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+var bot = new builder.UniversalBot(connector);
+bot.localePath(path.join(__dirname, './locale'));
+bot.set('storage', tableStorage);
+
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+  knowledgeBaseId: process.env.QnAKnowledgebaseId, 
+  subscriptionKey: process.env.QnASubscriptionKey});
+
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+  recognizers: [recognizer],
+  defaultMessage: 'No match! Try changing the query terms!',
+  qnaThreshold: 0.3}
+);
+
+bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
+
+bot.dialog('/', [
+  function (session){
+    session.send('Hejka');
+  }
+]);
+
+if (useEmulator) {
+  var restify = require('restify');
+  var server = restify.createServer();
+  server.listen(3978, function() {
+    console.log('test bot endpont at http://localhost:3978/api/messages');
+  });
+  server.post('/api/messages', connector.listen());    
+} else {
+  module.exports = connector.listen();
+}
